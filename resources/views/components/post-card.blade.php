310beb15e878aa7ec4a7a821b3c9e64be333128b
Jadcooma <jasperdecooman@gmail.com>
2022-10-17 14:06:02 +0200

    Hide unpublished posts

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,5 +1,6 @@
 @props(['post'])
 
+@if ($post->published)
 <article
     {{ $attributes->merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl']) }}>
     <div class="py-6 px-5 h-full flex flex-col">
@@ -49,3 +50,4 @@
         </div>
     </div>
 </article>
+@endif
